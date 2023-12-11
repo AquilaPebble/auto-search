ac7e50354838fb9f4f7bb46831b447882791be65
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2023-12-11 14:16:48 +1300

    removed stop mechanism, added actual words so you get points

--- auto-searcher.py
+++ auto-searcher.py
@@ -4,16 +4,12 @@ from random import randint
 from random import choice
 searchOn = False
 def enterKeys():
-    for o in range(randint(5,12)):
-        kb.press_and_release(choice([str(i) for i in range(0,10)] + list("abcdefghijklmnopqrstuvwxyz")))
+    for o in range(randint(0,15)):
+        kb.press_and_release(choice(['elephant','purple','jazz', 'breeze','carousel','whisper','marathon','quasar','umbrella','xylophone','nectar','galaxy','blizzard','trampoline','pyramid']) + ' ')
     kb.press_and_release('enter')
-    sleep(randint(3,5))
+    sleep(randint(4,5))
     kb.press_and_release('ctrl+w')
     sleep(0.1)
     kb.press_and_release('ctrl+t')
 while True:
-    if kb.is_pressed("`"):
-        searchOn = not searchOn
-        sleep(0.1)
-    if searchOn:
-        enterKeys()
+    enterKeys()
