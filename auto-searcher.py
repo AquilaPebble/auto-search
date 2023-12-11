cbcbed4290b7d1bb4f25722be210141fe7c067fa
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2023-12-11 14:24:41 +1300

    fixed keyboard error

--- auto-searcher.py
+++ auto-searcher.py
@@ -2,14 +2,20 @@ import keyboard as kb
 from time import sleep
 from random import randint
 from random import choice
-searchOn = False
+start = False
 def enterKeys():
-    for o in range(randint(0,15)):
-        kb.press_and_release(choice(['elephant','purple','jazz', 'breeze','carousel','whisper','marathon','quasar','umbrella','xylophone','nectar','galaxy','blizzard','trampoline','pyramid']) + ' ')
+    for i in range(randint(0,15)):
+        for o in choice(('elephant','purple','jazz','breeze','carousel','whisper','marathon','quasar','umbrella','xylophone','nectar','galaxy','blizzard','trampoline','pyramid')):
+            kb.press_and_release(o)
+        kb.press_and_release("space")
     kb.press_and_release('enter')
     sleep(randint(4,5))
     kb.press_and_release('ctrl+w')
     sleep(0.1)
     kb.press_and_release('ctrl+t')
 while True:
+    if kb.is_pressed("`"):
+        start = True
+        break
+while start:
     enterKeys()
