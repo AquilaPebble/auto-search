bff19a4f4c0a53aa081c8bb63879268dcc4a5875
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2023-12-11 14:32:55 +1300

    fixed time, catered to max 30 searches per day

--- auto-searcher.py
+++ auto-searcher.py
@@ -7,9 +7,10 @@ def enterKeys():
     for i in range(randint(0,15)):
         for o in choice(('elephant','purple','jazz','breeze','carousel','whisper','marathon','quasar','umbrella','xylophone','nectar','galaxy','blizzard','trampoline','pyramid')):
             kb.press_and_release(o)
+            sleep(randint(1,5) / 10)
         kb.press_and_release("space")
     kb.press_and_release('enter')
-    sleep(randint(5,10))
+    sleep(randint(10,15))
     kb.press_and_release('ctrl+w')
     sleep(0.1)
     kb.press_and_release('ctrl+t')
@@ -17,5 +18,5 @@ while True:
     if kb.is_pressed("`"):
         start = True
         break
-while start:
+for i in range(30):
     enterKeys()
